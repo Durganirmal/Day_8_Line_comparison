dca310635ce68768c043caf5945679a8e31a9036
Durga Nirmal <durganirmal123@gmail.com>
2022-01-03 19:13:28 +0530

    Added : Program for check equality of both line

--- src/com/Line_Comparison/Line_Comparison.java
+++ src/com/Line_Comparison/Line_Comparison.java
@@ -2,23 +2,40 @@ package com.Line_Comparison;
 
 import java.util.Scanner;
 public class Line_Comparison {
-
-	public static void main(String[] args) {
+	
+	public static int x,x1,x2,x3,y,y1,y2,y3;
+    public static double distance1,distance2;
+    public static void main(String[] args) {
 		System.out.println("Welcome To Line Comparison program");
 		input();
+		distance();
 	}
+    
 		 public static void input() {
-		        Scanner scanner = new Scanner(System.in);
+			 Scanner scanner = new Scanner(System.in);
 		        System.out.print("enter the value of x = ");
-		        int x = scanner.nextInt();
+		        x = scanner.nextInt();
 		        System.out.print("enter the value of x1 = ");
-		        int x1 = scanner.nextInt();
+		        x1 = scanner.nextInt();
+		        System.out.print("enter the value of x2 = ");
+		        x2 = scanner.nextInt();
+		        System.out.print("enter the value of x3 = ");
+		        x3 = scanner.nextInt();
 		        System.out.print("enter the value of y = ");
-		        int y = scanner.nextInt();
+		        y = scanner.nextInt();
 		        System.out.print("enter the value of y1 = ");
-		        int y1 = scanner.nextInt();
-
-		        double distance = Math.sqrt((x1 - x) * (x1 - x) + (y1 - y) * (y1 - y));
-		        System.out.println("Distance = " + distance);
+		        y1 = scanner.nextInt();
+		        System.out.print("enter the value of y2 = ");
+		        y2 = scanner.nextInt();
+		        System.out.print("enter the value of y3 = ");
+		        y3 = scanner.nextInt();
 		    }
-	}
+
+		        public static void distance() {
+		            distance1=Math.sqrt((x1-x)*(x1-x) + (y1-y)*(y1-y));
+		            System.out.println("Distance = " +distance1);
+		            distance2=Math.sqrt((x3-x2)*(x3-x2) + (y3-y2)*(y3-y2));
+		            System.out.println("Distance = " +distance2);
+
+		    }
+		}

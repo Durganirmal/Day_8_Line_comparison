0fee78c83ba70e342f3144b5817cfabb07c7ed97
Durga Nirmal <durganirmal123@gmail.com>
2022-01-03 19:06:09 +0530

    Added : Program for Calculation of length of line

--- src/com/Line_Comparison/Line_Comparison.java
+++ src/com/Line_Comparison/Line_Comparison.java
@@ -1,9 +1,24 @@
 package com.Line_Comparison;
 
+import java.util.Scanner;
 public class Line_Comparison {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome To Line Comparison program");
+		input();
 	}
+		 public static void input() {
+		        Scanner scanner = new Scanner(System.in);
+		        System.out.print("enter the value of x = ");
+		        int x = scanner.nextInt();
+		        System.out.print("enter the value of x1 = ");
+		        int x1 = scanner.nextInt();
+		        System.out.print("enter the value of y = ");
+		        int y = scanner.nextInt();
+		        System.out.print("enter the value of y1 = ");
+		        int y1 = scanner.nextInt();
 
-}
+		        double distance = Math.sqrt((x1 - x) * (x1 - x) + (y1 - y) * (y1 - y));
+		        System.out.println("Distance = " + distance);
+		    }
+	}
